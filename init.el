64ef7e9e54736affe37140babb15aecc0febc7ec
jerryzhao <jerry.zhao@thecarousell.com>
2018-05-09 00:16:15 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,140 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(require 'init-packages)
+
+;; turn on full screen 
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+;; hide tool bar
+(tool-bar-mode -1)
+
+;; turn on nyan mode
+(nyan-mode 1)
+
+;; swap meta and super key and change swith language
+(setq mac-option-modifier 'super)
+(setq mac-command-modifier 'meta)
+
+;; 关闭文件滑动控件
+(scroll-bar-mode -1)
+
+;; delete the selection
+(delete-selection-mode 1)
+
+
+;; add recent file
+
+(recentf-mode 1)
+(setq recentf-max-menu-item 10)
+
+
+;; 更改光标的样式（不能生效，解决方案见第二集）
+(setq cursor-type 'bar)
+
+;; 关闭启动帮助画面
+(setq inhibit-splash-screen 1)
+
+;; turn off backup files
+(setq make-backup-files nil) 
+
+
+;; 更改显示字体大小 16pt
+;; http://stackoverflow.com/questions/294664/how-to-set-the-font-size-in-emacs
+(set-face-attribute 'default nil :height 140)
+
+;; 快速打开配置文件
+(defun open-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+;; 这一行代码，将函数 open-init-file 绑定到 <f2> 键上
+(global-set-key (kbd "<f2>") 'open-init-file)
+
+
+
+;; add hook for elisp
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+;; add smartparens mode
+(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+
+
+
+
+
+(setq org-src-fontify-natively t)
+;; 设置默认 Org Agenda 文件目录
+(setq org-agenda-files '("~/org"))
+;; 设置 org-agenda 打开快捷键
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+(put 'scroll-left 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0.01)
+ '(company-minimum-prefix-length 1)
+ '(custom-safe-themes
+   (quote
+    ("6ac7c0f959f0d7853915012e78ff70150bfbe2a69a1b703c3ac4184f9ae3ae02" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" default)))
+ '(package-selected-packages
+   (quote
+    (gruvbox-theme nyan-mode company hungry-delete swiper counsel smartparens js2-mode nodejs-repl exec-path-from-shell monokai-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; jump file
+(global-set-key (kbd "C-h C-f") 'find-function)
+(global-set-key (kbd "C-h C-v") 'find-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
+
+;; abbrevmode auto complete
+(abbrev-mode t)
+(define-abbrev-table 'global-abbrev-table '(
+					    ;;demo
+					    ("8a" "aaaaaa")))
+;; org configuation
+(require 'org)
+
+
+
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "C-c C-r") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+(global-set-key (kbd "C-c g") 'counsel-git)
+(global-set-key (kbd "C-c j") 'counsel-git-grep)
+(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-x l") 'counsel-locate)
+(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+(define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
---
+++ lisp/init-packages.el
@@ -0,0 +1,92 @@
+(require 'cl)
+
+;; package management
+(require 'package)
+(add-to-list 'package-archives (cons "melpa" "http://melpa.org/packages/") t)
+
+;; Add Packages
+(defvar my/packages '( 
+                ;; --- Auto-completion ---
+                company
+                ;; --- Better Editor ---
+                hungry-delete
+                swiper
+                counsel
+                smartparens
+                ;; --- Major Mode ---
+                js2-mode
+		popwin
+                ;; --- Minor Mode ---
+                nodejs-repl
+                exec-path-from-shell
+                ;; --- Themes ---
+                gruvbox-theme
+                ;; solarized-theme
+                ) "Default packages")
+
+(setq package-selected-packages my/packages)
+
+(defun my/packages-installed-p ()
+     (loop for pkg in my/packages
+           when (not (package-installed-p pkg)) do (return nil)
+           finally (return t)))
+
+(unless (my/packages-installed-p)
+     (message "%s" "Refreshing package database...")
+     (package-refresh-contents)
+     (dolist (pkg my/packages)
+       (when (not (package-installed-p pkg))
+         (package-install pkg))))
+
+;; Find Executable Path on OS X
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+
+
+;; set hungry delete
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+
+
+;; add smartparens config 
+(require 'smartparens-config)
+
+
+
+;; popwin setting
+(require 'popwin)
+(popwin-mode t)
+
+
+
+;; highlight global line
+(global-hl-line-mode t)
+(require 'recentf)
+
+
+
+;; 显示行号
+(global-linum-mode 1)
+
+
+
+;; 开启全局 Company 补全
+(global-company-mode 1)
+
+
+
+;; ivy setting
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+
+
+
+;; load theme
+(load-theme 'gruvbox-dark-medium 1)
+
+
+
+(provide 'init-packages)
