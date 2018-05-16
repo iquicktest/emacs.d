9bacd07b86354084cf0565b59efb5e9d63d8c136
jerryzhao <jerry.zhao@thecarousell.com>
2018-05-16 14:56:14 +0800

    add config.org

--- custom.el
+++ custom.el
@@ -3,12 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
  '(company-idle-delay 0.01)
  '(company-meghanada-prefix-length 2)
  '(company-minimum-prefix-length 1)
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "6ac7c0f959f0d7853915012e78ff70150bfbe2a69a1b703c3ac4184f9ae3ae02" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8efj69017c68" default)))
+ '(elfeed-feeds
+   (quote
+    ("http://tech.163.com/special/000944OI/headlines.xml" "http://pragmaticemacs.com/feed/" "http://www.feng.com/rss.xml" "feed://www.feng.com/rss.xml" "feed://pragmaticemacs.com/feed/")))
+ '(evil-leader/in-all-states t)
+ '(evil-leader/leader "SPC")
  '(nyan-bar-length 20)
  '(package-selected-packages
    (quote
