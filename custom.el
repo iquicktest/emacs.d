9467f99d7d7087130fce5eb6a2facfa7c71c6c80
jerryzhao <jerry.zhao@thecarousell.com>
2018-06-03 12:35:32 +0800

    add elpy setting

--- custom.el
+++ custom.el
@@ -7,22 +7,30 @@
  '(company-idle-delay 0.01)
  '(company-meghanada-prefix-length 2)
  '(company-minimum-prefix-length 1)
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
  '(custom-safe-themes
    (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "6ac7c0f959f0d7853915012e78ff70150bfbe2a69a1b703c3ac4184f9ae3ae02" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8efj69017c68" default)))
+    ("42b9d85321f5a152a6aef0cc8173e701f572175d6711361955ecfb4943fe93af" "bc4b650c41b16b98166b35da94b366c6a9e1e7883bbf4937c897fb7bd05aa619" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "021720af46e6e78e2be7875b2b5b05344f4e21fad70d17af7acfd6922386b61e" "190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "6ac7c0f959f0d7853915012e78ff70150bfbe2a69a1b703c3ac4184f9ae3ae02" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8efj69017c68" default)))
  '(elfeed-feeds
    (quote
     ("http://tech.163.com/special/000944OI/headlines.xml" "http://pragmaticemacs.com/feed/" "http://www.feng.com/rss.xml" "feed://www.feng.com/rss.xml" "feed://pragmaticemacs.com/feed/")))
  '(evil-leader/in-all-states t)
  '(evil-leader/leader "SPC")
- '(nyan-bar-length 20)
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(nyan-bar-length 35)
+ '(nyan-mode nil)
  '(package-selected-packages
    (quote
-    (org-bullets org bongo company-lsp lsp-ui lsp-java gruvbox-theme nyan-mode company hungry-delete swiper counsel smartparens js2-mode nodejs-repl exec-path-from-shell monokai-theme)))
- '(pdf-tools-handle-upgrades nil))
+    (elpy lsp-python hc-zenburn-theme labburn-theme rainbow-identifiers rainbow-delimiters zenburn-theme org-bullets org bongo company-lsp lsp-ui lsp-java gruvbox-theme nyan-mode company hungry-delete swiper counsel smartparens js2-mode nodejs-repl exec-path-from-shell monokai-theme)))
+ '(pdf-tools-handle-upgrades nil)
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(sml/no-confirm-load-theme t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(mode-line ((t (:background "#2B2B2B" :foreground "#DCDCCC" :box (:line-width 4 :color "#2B2B2B"))))))
+ '(mode-line ((t (:background "#1c1c1c" :Foreground "#DCDCCC" :box (:line-width 4 :color "#1c1c1c"))))))
