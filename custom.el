f42d5e4325a3a0661297d5926d23f1dea6cc006b
jerryzhao <jerry.zhao@thecarousell.com>
2018-07-03 20:20:30 +0800

    add init.el

--- custom.el
+++ custom.el
@@ -11,7 +11,7 @@
  '(company-quickhelp-color-foreground "#DCDCCC")
  '(custom-safe-themes
    (quote
-    ("42b9d85321f5a152a6aef0cc8173e701f572175d6711361955ecfb4943fe93af" "bc4b650c41b16b98166b35da94b366c6a9e1e7883bbf4937c897fb7bd05aa619" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "021720af46e6e78e2be7875b2b5b05344f4e21fad70d17af7acfd6922386b61e" "190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "6ac7c0f959f0d7853915012e78ff70150bfbe2a69a1b703c3ac4184f9ae3ae02" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8efj69017c68" default)))
+    ("f27c3fcfb19bf38892bc6e72d0046af7a1ded81f54435f9d4d09b3bff9c52fc1" "62c81ae32320ceff5228edceeaa6895c029cc8f43c8c98a023f91b5b339d633f" "cd4d1a0656fee24dc062b997f54d6f9b7da8f6dc8053ac858f15820f9a04a679" "42b9d85321f5a152a6aef0cc8173e701f572175d6711361955ecfb4943fe93af" "bc4b650c41b16b98166b35da94b366c6a9e1e7883bbf4937c897fb7bd05aa619" "bcc6775934c9adf5f3bd1f428326ce0dcd34d743a92df48c128e6438b815b44f" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "021720af46e6e78e2be7875b2b5b05344f4e21fad70d17af7acfd6922386b61e" "190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8ef69017c68" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "6ac7c0f959f0d7853915012e78ff70150bfbe2a69a1b703c3ac4184f9ae3ae02" "c3d4af771cbe0501d5a865656802788a9a0ff9cf10a7df704ec8b8efj69017c68" default)))
  '(elfeed-feeds
    (quote
     ("http://tech.163.com/special/000944OI/headlines.xml" "http://pragmaticemacs.com/feed/" "http://www.feng.com/rss.xml" "feed://www.feng.com/rss.xml" "feed://pragmaticemacs.com/feed/")))
@@ -27,7 +27,7 @@
     ("~/Dropbox/org/routines.org" "~/Dropbox/org/task.org" "~/Dropbox/org/reading.org" "~/Dropbox/org/projects.org" "~/Dropbox/org/working.org" "~/Dropbox/org/inbox.org")))
  '(package-selected-packages
    (quote
-    (ein htmlize ivy-todo feature-mode gorepl-mode evil-collection hydra elpy lsp-python hc-zenburn-theme labburn-theme rainbow-identifiers rainbow-delimiters zenburn-theme org-bullets org bongo company-lsp lsp-ui lsp-java gruvbox-theme nyan-mode company hungry-delete swiper counsel smartparens js2-mode nodejs-repl exec-path-from-shell monokai-theme)))
+    (pyim ein htmlize ivy-todo feature-mode gorepl-mode evil-collection hydra elpy lsp-python hc-zenburn-theme labburn-theme rainbow-identifiers rainbow-delimiters zenburn-theme org-bullets org bongo company-lsp lsp-ui lsp-java gruvbox-theme nyan-mode company hungry-delete swiper counsel smartparens js2-mode nodejs-repl exec-path-from-shell monokai-theme)))
  '(pdf-tools-handle-upgrades nil)
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(sml/no-confirm-load-theme t))
--- init.el
+++ init.el
@@ -966,7 +966,8 @@ the frame and makes it a dedicated window for that buffer."
 (yas-global-mode 1)
 
 (setq org-publish-project-alist
-      '(("iquicktest"
+      '(
+	("iquicktest"
          ;; Path to org files.
          :base-directory "~/iquicktest.github.io/org"
          :base-extension "org"
@@ -978,7 +979,70 @@ the frame and makes it a dedicated window for that buffer."
          :headline-levels 4
          :html-extension "html"
          :body-only t
-         )))
+         )
+	("51music"
+         ;; Path to org files.
+         :base-directory "~/Dropbox/1millionDollars/51music.github.com/org/"
+         :base-extension "org"
+
+         ;; Path to Jekyll Posts
+         :publishing-directory "~/Dropbox/1millionDollars/51music.github.com/_posts/"
+         :recursive t
+         :publishing-function org-html-publish-to-html
+         :headline-levels 4
+         :html-extension "html"
+         :body-only t
+         )
+	)
+      )
+
+
+(use-package pyim
+  :ensure nil
+  :config
+  ;; 激活 basedict 拼音词库
+  (use-package pyim-basedict
+    :ensure nil
+    :config (pyim-basedict-enable))
+
+  (setq default-input-method "pyim")
+
+  ;; 我使用全拼
+  (setq pyim-default-scheme 'quanpin)
+
+  ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
+  ;; 我自己使用的中英文动态切换规则是：
+  ;; 1. 光标只有在注释里面时，才可以输入中文。
+  ;; 2. 光标前是汉字字符时，才能输入中文。
+  ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
+  (setq-default pyim-english-input-switch-functions
+                '(pyim-probe-dynamic-english
+                  pyim-probe-isearch-mode
+                  pyim-probe-program-mode
+                  pyim-probe-org-structure-template))
+
+  (setq-default pyim-punctuation-half-width-functions
+                '(pyim-probe-punctuation-line-beginning
+                  pyim-probe-punctuation-after-punctuation))
+
+  ;; 开启拼音搜索功能
+  (pyim-isearch-mode 1)
+
+  ;; 使用 pupup-el 来绘制选词框
+  (setq pyim-page-tooltip 'popup)
+
+  ;; 选词框显示5个候选词
+  (setq pyim-page-length 5)
+
+  ;; 让 Emacs 启动时自动加载 pyim 词库
+  (add-hook 'emacs-startup-hook
+            #'(lambda () (pyim-restart-1 t)))
+  :bind
+  (("M-j" . pyim-convert-code-at-point) ;与 pyim-probe-dynamic-english 配合
+   ("C-;" . pyim-delete-word-from-personal-buffer)))
+
+(global-set-key (kbd "C-\\") 'toggle-input-method)
+(define-key org-mode-map "\M-q" 'toggle-truncate-lines)
 
 
 
